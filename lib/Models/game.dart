4616fcd80f66fc59afd1c47bccd978dc9695be85
Oumayma <oumayma.cherif@esprit.tn>
2023-11-08 02:23:15 +0100

    s2

--- lib/Models/game.dart
+++ lib/Models/game.dart
@@ -1,11 +1,7 @@
 class Game {
-    //var 
-    final String name , image ;
-    final  int price ;
-    //constructeur 
-    const Game (this.name , this.image ,this.price );
-
-
-
-    
+  //var
+  final String name, image;
+  final int price;
+  //constructeur
+  const Game(this.name, this.image, this.price);
 }
---
+++ lib/screens/SignUpScreen/sign_up.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SignUpScree extends StatelessWidget {
+  const SignUpScree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Placeholder(
+      appBar: AppBar,
+      body: Padding(context
+      padding: const EdgeInsets.all(10.0),
+      child: ListView (children: [
+        //1 image 
+        Image.asset(name)
+        const SizedBox(height: 50,),
+        TextFormField ( decoration: const  InputDecoration(hintText:"Username" , prefixIcon: Icon(Icons.person ),
+        
+        
+        
+        )
+      ],
+
+      ),
+    );
+  }
+}
